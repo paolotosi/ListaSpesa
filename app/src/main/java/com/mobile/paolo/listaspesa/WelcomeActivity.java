4f3eaec8553ee40e0092e204f2c15b28ab007ffe
Paolo <p.tosi93@gmail.com>
2017-06-11 14:07:06 +0200

    Added style back-compatibility

--- app/src/main/java/com/mobile/paolo/listaspesa/WelcomeActivity.java
+++ app/src/main/java/com/mobile/paolo/listaspesa/WelcomeActivity.java
@@ -2,8 +2,9 @@ package com.mobile.paolo.listaspesa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
