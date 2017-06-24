de5883cfe444a9667199a22c3c3e854593c697dd
RevoCris92 <revocris92@gmail.com>
2017-06-24 20:06:39 +0200

    Non flasha più il farabutto.

--- app/src/main/java/com/mobile/paolo/listaspesa/SplashActivity.java
+++ app/src/main/java/com/mobile/paolo/listaspesa/SplashActivity.java
@@ -8,9 +8,18 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.transition.Fade;
+import android.transition.Transition;
 import android.util.Pair;
 import android.view.View;
+import android.view.Window;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.animation;
 
 /*
  * -- SplashActivity --
@@ -73,6 +82,10 @@ public class SplashActivity extends Activity {
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
+                TranslateAnimation animationCleaner = new TranslateAnimation(0.0f, 0.0f, 0.0f, 0.0f);
+                animationCleaner.setDuration(aSecond);
+                findViewById(R.id.splashView).startAnimation(animationCleaner);
+                findViewById(R.id.splashView).startAnimation(animationCleaner);
                 transitionToNextActivity();
             }
         });
@@ -95,9 +108,18 @@ public class SplashActivity extends Activity {
     public void transitionToNextActivity()
     {
         // Shared element transition
+        View statusBar = findViewById(android.R.id.statusBarBackground);
+        View navigationBar = findViewById(android.R.id.navigationBarBackground);
+        View background = findViewById(android.R.id.background);
         Pair sharedElements = new Pair<View, String>(logoWhite, "logo_shared");
-        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this, sharedElements);
+        List<Pair<View, String>> pairs = new ArrayList<>();
+        pairs.add(Pair.create(statusBar, Window.STATUS_BAR_BACKGROUND_TRANSITION_NAME));
+        pairs.add(Pair.create(navigationBar, Window.NAVIGATION_BAR_BACKGROUND_TRANSITION_NAME));
+        //pairs.add(Pair.create(background, "Background"));
+        pairs.add(sharedElements);
+
+        Bundle options = ActivityOptions.makeSceneTransitionAnimation(this, pairs.toArray(new Pair[pairs.size()])).toBundle();
         Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
-        startActivity(intent, options.toBundle());
+        startActivity(intent, options);
     }
 }
--- app/src/main/java/com/mobile/paolo/listaspesa/WelcomeActivity.java
+++ app/src/main/java/com/mobile/paolo/listaspesa/WelcomeActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewTreeObserver;
 
 /*
     -- SplashActivity --
@@ -18,6 +19,8 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        getWindow().setEnterTransition(null);
 
         // Login
         findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
